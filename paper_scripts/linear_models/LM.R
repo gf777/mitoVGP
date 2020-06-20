286aa299707a7a23d9cbbbebfba90176b98ec0b7
Giulio <gformenti@rockefeller.edu>
2020-06-20 17:57:08 -0400

    updated lm

--- paper_scripts/linear_models/LM.R
+++ paper_scripts/linear_models/LM.R
@@ -1,43 +1,55 @@
+# load libraries
 library(dplyr)
 library(ggplot2)
-library(broom)
 library(lawstat)
-library("readxl")
+library(readxl)
+library(lmPerm)
+library(multcomp)
 
+# set working directory
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
-mitoVGP <- read_excel("../Supplementary Tables/Supplementary Tables.xlsx", sheet = "ST1")
+# import data
 
-is.factor(mitoVGP$`Size selection kbp (Pacbio)`)
-is.numeric(mitoVGP$`Total raw data (Gbp)`)
+mitoVGP.All <- read_excel("../Supplementary Tables/Supplementary Tables.xlsx", sheet = "ST1")
 
-mitoVGP <- mutate(mitoVGP, `Size selection cutoff` = ifelse(`Size selection kbp (Pacbio)` > 20, "Above 20", "Below 20"))
+names(mitoVGP.All)
+# select relevant variables for analyses
+mitoVGP <- mitoVGP.All[,c(1, 7, 8, 9, 11, 13, 15, 17, 25, 28)]
 
-mitoVGP$`Size selection cutoff` <- as.factor(mitoVGP$`Size selection cutoff`)
-is.factor(mitoVGP$`Size selection cutoff`)
+# set factors and numbers
+mitoVGP <- mitoVGP %>% mutate_if(is.character, as.factor)
 
-out1<-with(mitoVGP, table(`Tissue type (Pacbio)`, `Taxonomic group`))
-out2<-with(mitoVGP, table(`Tissue type (Pacbio)`, Success))
-out3<-with(mitoVGP, table(`Size selection kbp (Pacbio)`, Success))
-out4<-with(mitoVGP, table(`Size selection cutoff`, Success))
+mitoVGP <- mutate(mitoVGP, Size.selection.cutoff = ifelse(Size.selection.kbp.Pacbio > 20,
+                                                          "Above 20", "Below 20"))
+str(mitoVGP)
+# mitoVGP$Taxonomic.group <- as.factor(mitoVGP$Taxonomic.group)
+# mitoVGP$Tissue.type.Pacbio <- as.factor(mitoVGP$Tissue.type.Pacbio)
+# mitoVGP$DNA.extraction.Pacbio <- as.factor(mitoVGP$DNA.extraction.Pacbio)
+# mitoVGP$Library.prep.fragmentation.Pacbio <- as.factor(mitoVGP$Library.prep.fragmentation.Pacbio)
+# mitoVGP$Library.prep.Pacbio <- as.factor(mitoVGP$Library.prep.Pacbio)
 
-out5<-with(mitoVGP, table(`VGP repeat type`, `Tissue type (Pacbio)`))
-out6<-with(mitoVGP, table(`VGP duplicated genes`, `Tissue type (Pacbio)`))
 
-fisher.test(out5,simulate.p.value=TRUE)
-fisher.test(out6,simulate.p.value=TRUE)
+# produce and save result tables
+out1 <- with(mitoVGP, table(Tissue.type.Pacbio, Taxonomic.group))
+out2 <- with(mitoVGP, table(Tissue.type.Pacbio, Success))
+out3 <- with(mitoVGP, table(Size.selection.kbp.Pacbio, Success))
+out4 <- with(mitoVGP, table(Size.selection.cutoff, Success))
 
 write.table(out1, file='../Supplementary Tables/ST3.tsv', quote=FALSE, sep='\t')
 write.table(out2, file='../Supplementary Tables/ST5.tsv', quote=FALSE, sep='\t')
 write.table(out3, file='../Supplementary Tables/ST6a.tsv', quote=FALSE, sep='\t')
 write.table(out4, file='../Supplementary Tables/ST6b.tsv', quote=FALSE, sep='\t')
 
+# code for Figure 3 in the extended data
+
 mitoVGP <- mitoVGP %>%
   mutate(Success = ifelse(Success == "no",0,1))
 
 png("../Extended Data Fig. 3/Extended Data Fig. 3.png", width = 2000, height = 750)
 
-ggplot(mitoVGP, aes(x=reorder(`Tissue type (Pacbio)`, -`Available Pacbio mtDNA reads`),  y=`Available Pacbio mtDNA reads`)) + 
+ggplot(mitoVGP, aes(x=reorder(Tissue.type.Pacbio, -Available.Pacbio.mtDNA.reads),  
+                    y=Available.Pacbio.mtDNA.reads)) + 
   geom_boxplot(outlier.colour="black", outlier.shape=8,
                outlier.size=4)+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
@@ -50,27 +62,25 @@ ggplot(mitoVGP, aes(x=reorder(`Tissue type (Pacbio)`, -`Available Pacbio mtDNA r
     axis.text = element_text(family = "Arial",
                              size = rel(4), colour = "black"),
     axis.ticks.length = unit(.50, "cm"),
-    axis.ticks = element_line(colour = "black", size=(0.6)),
+    axis.ticks = element_line(colour = "black", size=(0.6))
   )
 
 dev.off()
 
-plot(mitoVGP$Success, mitoVGP$`Available Pacbio mtDNA reads`)
-plot(mitoVGP$`Taxonomic group`, mitoVGP$`Available Pacbio mtDNA reads`)
+# other plots
+plot(Success ~ Available.Pacbio.mtDNA.reads, data = mitoVGP)
+boxplot(Available.Pacbio.mtDNA.reads ~ Taxonomic.group, data = mitoVGP)
 
-#Q <- quantile(mitoVGP$N.Pacbio.reads, probs=c(.25, .75), na.rm = FALSE)
-#iqr <- IQR(mitoVGP$N.Pacbio.reads)
-#mitoVGP_nooutliers<- subset(mitoVGP, mitoVGP$N.Pacbio.reads > (Q[1] - 1.5*iqr) & mitoVGP$N.Pacbio.reads < (Q[2]+1.5*iqr))
+# mod.mitoVGP <- glm(Success ~ `Available Pacbio mtDNA reads`, family="binomial", data=mitoVGP)
+# summary(mod.mitoVGP)
+# anova(mod.mitoVGP, test="Chi")
 
-mod.mitoVGP <- glm(Success ~ `Available Pacbio mtDNA reads`, family="binomial", data=mitoVGP)
-summary(mod.mitoVGP)
-anova(mod.mitoVGP, test="Chi")
 
 png("../Extended Data Fig. 2/Extended Data Fig. 2.png", width = 2000, height = 750)
 
 mitoVGP %>%
-  mutate(size = ifelse(`Available Pacbio mtDNA reads` < 1, "0", ">0")) %>%
-  ggplot(aes(x = `Available Pacbio mtDNA reads`, y = Success, colour = size))  +
+  mutate(size = ifelse(Available.Pacbio.mtDNA.reads < 1, "0", ">0")) %>%
+  ggplot(aes(x = Available.Pacbio.mtDNA.reads, y = Success, colour = size))  +
   stat_sum() +
   geom_smooth(method = "glm", formula = y ~ x, se = FALSE,
               method.args = list(family = binomial()), colour = "gray")+
@@ -95,62 +105,103 @@ mitoVGP %>%
 
 dev.off()
 
-#r2
-nullmod <- glm(Success ~ 0, family="binomial", data=mitoVGP)
-1-logLik(mod.mitoVGP)/logLik(nullmod)
+# #r2
+# nullmod <- glm(Success ~ 0, family="binomial", data=mitoVGP)
+# 1-logLik(mod.mitoVGP)/logLik(nullmod)
+# 
+# #dist
+# hist(mitoVGP$`Available Pacbio mtDNA reads`)
+# hist(sqrt(mitoVGP$`Available Pacbio mtDNA reads`))
 
-#dist
-hist(mitoVGP$`Available Pacbio mtDNA reads`)
-hist(sqrt(mitoVGP$`Available Pacbio mtDNA reads`))
+# Chi-square test for associtation of assembly success with 
+# availability of long reads
+Tab2 <- with(mitoVGP,table(Success, I(Available.Pacbio.mtDNA.reads>0)))
+chisq.test(Tab2)
+# possible alternative tests
+fisher.test(Tab2)
+chisq.test(Tab2, simulate.p.value = TRUE, B = 10000)
 
-filtered_mitVGP<- mitoVGP %>% group_by(`Tissue type (Pacbio)`) %>% filter(n() > 3) %>%
-                              group_by(`Taxonomic group`) %>% filter(n() > 3) %>%
-                              group_by(`DNA extraction (Pacbio)`) %>% filter(n() > 3) %>%
-                              group_by(`Library prep - fragmentation (Pacbio)`) %>% filter(n() > 3) %>%
-                              group_by(`Library prep (Pacbio)`) %>% filter(n() > 3)
-  
+# Chi-square test for the association between tissue type and taxonomic group
+chisq.test(out1)
+# possible alternative tests
+chisq.test(out1, simulate.p.value = TRUE, B = 10000)
+# fisher.test(out1) does not work
 
-#N reads by tissue type, taxonomic group, size selection, DNA extraction, Library prep fragmentation, library prep
-mod.mitoVGP <- lm(`Available Pacbio mtDNA reads` ~ `Tissue type (Pacbio)` + `Taxonomic group` + `Size selection kbp (Pacbio)` + `DNA extraction (Pacbio)` + `Library prep - fragmentation (Pacbio)` + `Library prep (Pacbio)` + `Total raw data (Gbp)`, data=filtered_mitVGP)
+# keep only categories with N > 3
+filtered_mitoVGP <- mitoVGP %>% group_by(Tissue.type.Pacbio) %>% filter(n() > 3) %>%
+                                group_by(Taxonomic.group) %>% filter(n() > 3) %>%                               
+                                group_by(DNA.extraction.Pacbio) %>% filter(n() > 3) %>%
+                                group_by(Library.prep.fragmentation.Pacbio) %>% filter(n() > 3) %>%
+                                group_by(Library.prep.Pacbio) %>% filter(n() > 3)
 
-summary(mod.mitoVGP)
-af <- anova(mod.mitoVGP)
-af
-afss <- af$"Sum Sq"
-ml<-cbind(af,PctExp=afss/sum(afss)*100)
-ml
+# turn into factor and remove unused levels
+filtered_mitoVGP <- filtered_mitoVGP %>% mutate_if(is.character, as.factor)
+filtered_mitoVGP <- filtered_mitoVGP %>% mutate_if(is.factor, droplevels)
+# for some reason some columns sometimes are not converted into factor
+filtered_mitoVGP$Taxonomic.group <- as.factor(filtered_mitoVGP$Taxonomic.group)
+filtered_mitoVGP$Tissue.type.Pacbio <- as.factor(filtered_mitoVGP$Tissue.type.Pacbio)
+filtered_mitoVGP$Library.prep.fragmentation.Pacbio <- as.factor(filtered_mitoVGP$Library.prep.fragmentation.Pacbio)
 
-write.table(ml, file='../Supplementary Tables/ST4.tsv', quote=FALSE, sep='\t')
+# Chi-square test for the association between tissue type and taxonomic group in the filtered dataset
+out1_filt <- with(filtered_mitoVGP, table(Tissue.type.Pacbio, Taxonomic.group))
+chisq.test(out1_filt)
+# possible alternative tests
+chisq.test(out1_filt, simulate.p.value = TRUE, B = 10000)
+# fisher.test(out1_filt) does not work
 
-mean(mod.mitoVGP$residuals)
-acf(mod.mitoVGP$residuals)
-lawstat::runs.test(mod.mitoVGP$residuals)
+mod.mitoVGP <- lm(Available.Pacbio.mtDNA.reads ~ Tissue.type.Pacbio + Taxonomic.group + 
+                    Size.selection.kbp.Pacbio + DNA.extraction.Pacbio + 
+                    Library.prep.fragmentation.Pacbio + 
+                    Library.prep.Pacbio + Total.raw.data.Gbp, data=filtered_mitoVGP)
 
-alias(mod.mitoVGP)
-
-ggplot(mod.mitoVGP, aes(`Size selection kbp (Pacbio)`, `Available Pacbio mtDNA reads`)) +
-  geom_point() +
-  stat_smooth(method = lm, se = FALSE) +
-  geom_segment(aes(xend = `Size selection kbp (Pacbio)`, yend = .fitted), color = "red", size = 0.3)
-
+# check model assumptions
 par(mfrow = c(2, 2))
 plot(mod.mitoVGP)
 
-plot(mod.mitoVGP, 4, id.n = 5)
+par(mfrow = c(1, 1))
+acf(mod.mitoVGP$residuals)
 
-model.diag.metrics <- augment(mod.mitoVGP)
-head(model.diag.metrics)
+mean(mod.mitoVGP$residuals)
+lawstat::runs.test(mod.mitoVGP$residuals)
 
-model.diag.metrics <- model.diag.metrics %>%
-  mutate(index = 1:nrow(model.diag.metrics)) %>%
-  select(index, everything(), -.se.fit, -.sigma)
-
-#model using size selection with 20 kbp cutoff
-mod.mitoVGP <- lm(`Available Pacbio mtDNA reads` ~ `Tissue type (Pacbio)` + `Taxonomic group` + `Size selection cutoff` + `DNA extraction (Pacbio)` + `Library prep - fragmentation (Pacbio)` + `Library prep (Pacbio)` + `Total raw data (Gbp)`, data=filtered_mitVGP)
-
+# summary with amount of explained variance
 summary(mod.mitoVGP)
 af <- anova(mod.mitoVGP)
 af
 afss <- af$"Sum Sq"
-print(cbind(af,PctExp=afss/sum(afss)*100))
+ml<-cbind(af, PctExp=afss/sum(afss)*100)
+ml
 
+# check significance with permutation approach
+mod.mitoVGP.perm <- lm(Available.Pacbio.mtDNA.reads ~ Tissue.type.Pacbio + Taxonomic.group + 
+                         Size.selection.kbp.Pacbio + DNA.extraction.Pacbio + 
+                         Library.prep.fragmentation.Pacbio + 
+                         Library.prep.Pacbio + Total.raw.data.Gbp, data=filtered_mitoVGP)
+summary(mod.mitoVGP.perm)
+anova(mod.mitoVGP.perm)
+
+
+# post-hoc tests
+Post.hoc.taxon <- glht(mod.mitoVGP,linfct=mcp(Taxonomic.group = "Tukey"))
+summary(Post.hoc.taxon)
+
+Post.hoc.tissue <- glht(mod.mitoVGP, linfct=mcp(Tissue.type.Pacbio = "Tukey"))
+summary(Post.hoc.tissue)
+
+Post.hoc.fragmentation <- glht(mod.mitoVGP,linfct=mcp(Library.prep.fragmentation.Pacbio = "Tukey"))
+summary(Post.hoc.fragmentation)
+
+# write results
+# write.table(ml, file='../Supplementary Tables/ST4.tsv', quote=FALSE, sep='\t')
+
+# 
+# ggplot(mod.mitoVGP, aes(Size.selection.kbp.Pacbio, Available.Pacbio.mtDNA.reads)) +
+#   geom_point() +
+#   stat_smooth(method = lm, se = FALSE, formula = y ~ x) +
+#   geom_segment(aes(xend = Size.selection.kbp.Pacbio, yend = .fitted), color = "red", size = 0.3)
+
+# Chi-square test for the association between size selection and success
+chisq.test(out4)
+# possible alternative tests
+chisq.test(out4, simulate.p.value = TRUE, B = 10000)
+fisher.test(out4) 
